9bf737ca90f78bdd3d4a0865cb6c74a1ba45d471
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2023-08-27 01:51:38 +0700

    Update createcluster.sh

--- createcluster.sh
+++ createcluster.sh
@@ -109,20 +109,8 @@ export OS_IDENTITY_API_VERSION=3
 openstack server create --flavor ${WORKER_FLAVOR} --image "Worker Image Ubuntu 22.04" --network ${NETWORK} --security-group kamaji-rules --availability-zone ${AVAILABILITY_ZONE} --key-name remote-server --min ${COUNT} --max ${COUNT} --user-data script.sh "${TENANT_NAME}-${TENANT_VERSION}-worker" > /dev/null 2>&1
 kubectl --kubeconfig=${TENANT_NAME}.kubeconfig apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.1/manifests/calico.yaml > /dev/null 2>&1
 
-while true; do 
-  #STATUS=$(kubectl --kubeconfig=${TENANT_NAME}.kubeconfig get nodes --no-headers | grep "${TENANT_NAME}-${TENANT_VERSION}-worker-1" | awk '{print $2}')
-  STATUS=$(kubectl --kubeconfig=${TENANT_NAME}.kubeconfig get deploy -n kube-system | grep coredns | awk '{print $4}')
-  
-  
-#case "$STATUS" in
-#    "Ready")
-#      echo "Create WORKER SUCCESS"
-#      break
-#      ;;
-#    *)
-#  esac
-#done
-
+while true; do  
+  STATUS=$(kubectl --kubeconfig=${TENANT_NAME}.kubeconfig get deploy -n kube-system | grep coredns | awk '{print $4}') #Mengambil parameter deployment coredns, kedepannya parameter yang diambil yaitu, pod (running), node (ready)
 case "$STATUS" in
     "2")
       echo "Create WORKER SUCCESS"
