1a7c9d15749016c767540c132aed231d15ff390f
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2023-08-26 16:42:22 +0700

    Update kamajiscript.sh

--- kamajiscript.sh
+++ kamajiscript.sh
@@ -78,7 +78,8 @@ cat << EOF | tee script.sh > /dev/null 2>&1
 #cloud-config
 debug: True
 runcmd:
- - bash <(curl -s https://raw.githubusercontent.com/teghitsugaya/kamaji/main/v1.26.1)
+ - wget https://raw.githubusercontent.com/teghitsugaya/kamaji/main/${TENANT_VERSION}
+ - sudo sh ${TENANT_VERSION}
  - $JOIN_CMD
 EOF
 
