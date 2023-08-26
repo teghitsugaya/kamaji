355597a1ade86ddbd07cfbae6ec4aa48cdbbddbe
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2023-08-26 17:00:46 +0700

    Update kamajiscript.sh

--- kamajiscript.sh
+++ kamajiscript.sh
@@ -8,7 +8,7 @@ export KAMAJI_NAMESPACE=default
 
 #tenant cluster parameters
 export TENANT_NAMESPACE=default
-export TENANT_NAME=kube-126
+export TENANT_NAME=kube-2-126
 #Version Available = 1.27.0, 1.26.7, 1.25.12
 export TENANT_VERSION=v1.26.1
 
@@ -18,7 +18,7 @@ export WORKER_VERSION=1.26.1
 export WORKER_FLAVOR=GP.2C4G
 export AVAILABILITY_ZONE=AZ_Public01_DC3
 export NETWORK=Public_Subnet02_DC3
-export COUNT=1
+export COUNT=2
 
 echo "Deploy Cluster Kubernetes"
 echo "Cluster Name: ${TENANT_NAME}"
