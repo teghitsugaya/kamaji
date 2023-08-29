311ef219085bc9eefb4c4727808f642c4049622e
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2023-08-29 19:26:20 +0700

    Update addworker.sh

--- addworker.sh
+++ addworker.sh
@@ -22,16 +22,7 @@ export NETWORK=Public_Subnet02_DC2
 export COUNT=3
 
 #Proejct Tenant Parameters
-export OS_AUTH_URL=https://jktosp-horizon.dcloud.co.id/identity/v3/
-export OS_PROJECT_ID=8b39b22b07e644c5996ccb4ca196fb06
-export OS_PROJECT_NAME="Cloud Development"
-export OS_USER_DOMAIN_NAME="Default"
-export OS_PROJECT_DOMAIN_ID="default"
-export OS_USERNAME="teguh.imanto"
-export OS_PASSWORD=D4t4c0mm@2023!!!
-export OS_REGION_NAME="RegionOne"
-export OS_INTERFACE=public
-export OS_IDENTITY_API_VERSION=3
+. ~/cloud_development-openrc.sh
 
 kubectl get secrets -n ${TENANT_NAMESPACE} ${TENANT_NAME}-admin-kubeconfig -o json \
   | jq -r '.data["admin.conf"]' \
