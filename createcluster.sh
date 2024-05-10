dabd390bead29f697b228d6fe1df7cd77c92ccdb
teghitsugaya <44857533+teghitsugaya@users.noreply.github.com>
2024-05-10 14:54:30 +0700

    Update createcluster.sh

--- createcluster.sh
+++ createcluster.sh
@@ -42,7 +42,7 @@ metadata:
   name: ${TENANT_NAME}
   namespace: ${TENANT_NAMESPACE}
 spec:
-  dataStore: kamaji-etcd
+  dataStore: kamaji-etcd-2
   controlPlane:
     deployment:
       replicas: 3
